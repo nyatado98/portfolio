dfad8aa2d0be6ddecdcc76feb7c62f828b0646b8
nyatado98 <danndong080@gmail.com>
2023-08-03 16:28:48 +0300

    new changes in portfolio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,11 +8,22 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css">
 	<link rel="stylesheet" type="text/css" href="{{asset('css/text.css')}}">
+
+	  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <!-- <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/text.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
 		<div class="heade">
-			<a href="#" class="home" style="text-decoration: none" onmouseover="this.style.color='white'" onmouseout="this.style.color='lime'">DanTECH</a>
+			<a href="{{url('/')}}" class="home" style="text-decoration: none" onmouseover="this.style.color='white'" onmouseout="this.style.color='lime'">DanTECH</a>
 		</div>
 		 <button class="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -74,54 +85,122 @@
 	<ul style="color:white;" class="font-weight-bold"></ul>
 	</div>
     {{-- projects article part --}}
-    <div class="container-fluid" id="projects">
+    <div class="container-fluid portfolio" id="projects">
         <h5 class="text-center font-weight-bold text-white mt-5" style="font-size:25px;text-transform:uppercase;text-decoration:underline">The Follwoing are the projects that i have worked on and some are current on the process</h5>
         <div class="row">
         <div class="card-header col-md-4">
             <h5 class="text-center text-white">Cyber Management System</h5>
             <div class="card-body">
+            <div class="card-body portfolio-wrap">
             <img src="{{asset('images/cyber.png')}}" class="img-fluid" alt="">
             {{-- onmouseover="this.style.opacity='.5'" --}}
+            <div class="portfolio-info">
+            <h4>Cyber Management System</h4>
+            <p>Web system</p>
+            <div class="portfolio-links">
+              <a href="images/rental.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 1"><i class="bx bx-plus"></i></a>
+              <a href="projects.php" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
         </div>
         </div>
+    </div>
         <div class="col-md-4 mt-3">
             <h5 class="text-center text-white">Rental Management System</h5>
-            <div class="card-body">
+            <div class="card-body portfolio-wrap">
             <img src="images/rental.png" class="img-fluid" alt="">
+             <div class="portfolio-info">
+            <h4>Rental Management System</h4>
+            <p>Web system</p>
+            <div class="portfolio-links">
+              <a href="images/rental.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 1"><i class="bx bx-plus"></i></a>
+              <a href="projects.php" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
         </div>
         </div>
         <div class="col-md-4 mt-3">
             <h5 class="text-center text-white">High School Website</h5>
             <div class="card-body">
+            <div class="card-body portfolio-wrap">
             <img src="images/school.png" class="img-fluid" alt="">
+            <div class="portfolio-info">
+            <h4>High School Website</h4>
+            <p>Web system</p>
+            <div class="portfolio-links">
+              <a href="images/school.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 1"><i class="bx bx-plus"></i></a>
+              <a href="projects.php" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
         </div>
         </div>
+    </div>
         <div class="col-md-4 mt-3">
             <h5 class="text-center text-white">School mangement System</h5>
             <div class="card-body">
+            <div class="card-body portfolio-wrap">
             <img src="images/project.png" class="img-fluid" alt="">
+             <div class="portfolio-info">
+            <h4>School Management System</h4>
+            <p>Web system</p>
+            <div class="portfolio-links">
+              <a href="images/project.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 1"><i class="bx bx-plus"></i></a>
+              <a href="projects.php" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
         </div>
         </div>
+    </div>
         <div class="col-md-4 mt-3">
             <h5 class="text-center text-white">Betting site</h5>
             <div class="card-body">
+            <div class="card-body portfolio-wrap">
             <img src="images/betting.PNG" class="img-fluid" alt="">
+             <div class="portfolio-info">
+            <h4>Betting Site</h4>
+            <p>Web system</p>
+            <div class="portfolio-links">
+              <a href="images/betting.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 1"><i class="bx bx-plus"></i></a>
+              <a href="projects.php" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
         </div>
         </div>
+    </div>
         <div class="col-md-4 mt-3">
             <h5 class="text-center text-white">Portfolio</h5>
             <div class="card-body">
+            <div class="card-body portfolio-wrap">
             <img src="images/portfolio.PNG" class="img-fluid" alt="">
+             <div class="portfolio-info">
+            <h4>Portfolio</h4>
+            <p>Web system</p>
+            <div class="portfolio-links">
+              <a href="images/portfolio.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 1"><i class="bx bx-plus"></i></a>
+              <a href="projects.php" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
         </div>
         </div>
+    </div>
         <div class="col-md-4 mt-3">
             <h5 class="text-center text-white">Airline management system</h5>
             <div class="card-body">
+            <div class="card-body portfolio-wrap">
             <img src="images/air.png" class="img-fluid" alt="">
+             <div class="portfolio-info">
+            <h4>Airline Management System</h4>
+            <p>Web system</p>
+            <div class="portfolio-links">
+              <a href="images/air.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 1"><i class="bx bx-plus"></i></a>
+              <a href="projects.php" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
         </div>
         </div>
     </div>
     </div>
+</div>
 	<div class="container-fluid col-md-10" id="contacts">
         <h4 class="text-center font-weight-bold text-white mt-5" style="font-size:25px;text-transform:uppercase;text-decoration:underline">Contact me Here</h4>
 		<div class="row">
